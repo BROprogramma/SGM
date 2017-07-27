c374cdfb6cea230ab72d293ad1e356bdd6bf1d3e
Linda van den Brink <l.vandenbrink@geonovum.nl>
2017-07-27 16:39:43 +0200

    adding biblio entries

--- config.js
+++ config.js
@@ -41,7 +41,77 @@ var respecConfig = {
   github: "https://github.com/BROprogramma/SGM",
   //previousPublishDate: "2017-01-01",
   //previousStatus: "GN-CV",
-  //localBiblio: {
+  localBiblio: {
+    "Bakker-Schelling1989": {
+      title:
+        "Systeem voor Bodemclassificatie voor Nederland; De hogere niveaus. ",
+      authors: ["Bakker, H. de", "Schelling, J"],
+      date: "1989",
+      publisher: "Wageningen Pudoc",
+      status:
+        "Tweede, gewijzigde druk, bewerkt door D.J. Brus en C. van Wallenburg",
+    },
+    "Cate-etal1995": {
+      title:
+        "Handleiding bodemgeografisch onderzoek; richtlijnen en voorschriften. Deel A: Bodem. ",
+      authors: [
+        "Cate, J.A.M. ten",
+        "Holst, A.F. van",
+        "Kleijer, H",
+        "Stolp, J",
+      ],
+      date: "1995",
+      publisher: "Wageningen, Staring Centrum",
+      id: "Technisch Document 19A",
+    },
+    "Gruijter-etal2004": {
+      title:
+        "Grondwater opnieuw in kaart. Methodiek voor de actualisering van grondwaterstandsinformatie en perceelclassificatie naar uitspoelingsgevoeligheid voor nitraat.",
+      authors: [
+        "Gruijter, J.J. de",
+        "Horst, J.B.F. van der",
+        "Heuvlink, G.B.M.",
+        "Knotters, M.",
+        "Hoogland, T",
+      ],
+      date: "2004",
+      publisher: "Wageningen, Alterra",
+      id: "rapport 915",
+    },
+    Heesen1971: {
+      title: "De weergave van het grondwaterstandsverloop op bodemkaarten. ",
+      authors: ["Heesen, H.C. van"],
+      date: "1971",
+      publisher: "Boor en Spade 17, 127-149.",
+    },
+    "Hoogland-etal2014": {
+      title:
+        "Actualisatie van de grondwaterrappenkaart van holoceen Nederland.",
+      authors: [
+        "Hoogland, T.",
+        "Knotters, M.",
+        "Pleijter, M.",
+        "Walvoort, D.J.J",
+      ],
+      date: "2014",
+      publisher: "Wageningen, Alterra",
+      id: "Rapport 2612",
+    },
+    "Kekem-etal2003": {
+      title:
+        "Uitspoelingsgevoelige gronden op de kaart; werkwijze en resultaten. ",
+      authors: ["Kekem,, A.J. van", "Hoogland, T", "Horst, J.B.F. van der"],
+      date: "2003",
+      publisher: "Wageningen, Alterra",
+      id: "Rapport 1080",
+    },
+    Simonson1968: {
+      title: "Concept of soil. ",
+      authors: ["Simonson, R.W."],
+      date: "1968",
+      publisher: "Advances in Agronomy 20, 1-47",
+    },
+  },
   //  "ISO19136": {
   //    "href": "https://www.iso.org/standard/32554.html",
   //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
