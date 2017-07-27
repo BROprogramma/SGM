9127282497e7343b3b80d15d71e1fb9fae138a94
Linda van den Brink <l.vandenbrink@geonovum.nl>
2017-07-27 14:08:53 +0200

    added dummy repo files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# SGM
+# Bodem- en grondwatertrappenkaart (Soil and groundwater map / SGM)
+
+De gegevenscatalogus voor dit informatiemodel wordt in dit Github repository ontwikkeld.
+
+De versie in ontwikkeling is te bekijken op https://broprogramma.github.io/SGM/
---
+++ config.js
@@ -0,0 +1,53 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "IM",
+  publishDate: "2017-07-07",
+  editors: [
+    {
+      name: "Linda van den Brink",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl",
+    },
+  ],
+  authors: [
+    {
+      name: "Nanny Heidema",
+      company: "Wageningen University & Research",
+      companyURL: "https://www.wur.nl",
+    },
+    {
+      name: "Gilbert Maas",
+      company: "Wageningen University & Research",
+      companyURL: "https://www.wur.nl",
+    },
+    {
+      name: "Folkert de Vries",
+      company: "Wageningen University & Research",
+      companyURL: "https://www.wur.nl",
+    },
+    {
+      name: "Jandirk Bulens",
+      company: "Wageningen University & Research",
+      companyURL: "https://www.wur.nl",
+    },
+    {
+      name: "Wies Vullings",
+      company: "Wageningen University & Research",
+      companyURL: "https://www.wur.nl",
+    },
+  ],
+  shortName: "bro-sgm",
+  pubDomain: "bro",
+  github: "https://github.com/BROprogramma/SGM",
+  //previousPublishDate: "2017-01-01",
+  //previousStatus: "GN-CV",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
